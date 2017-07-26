3f984b2e5f02d9b7c70db3416a7e76bfdcd5d0a7
jessicaacove <jessicacove@Jessicas-MacBook.local>
2017-07-26 13:49:43 -0400

    moved dist

--- app.js
+++ app.js
@@ -65,6 +65,14 @@ app.use('/', myInvestmentRoutes);
 const myLenderRoutes = require('./routes/lender-routes');
 app.use('/', myLenderRoutes);
 
+// ____________Middleware________________________
+
+app.use((req, res, next) => {
+  // if no routes match, send them the angular HTML
+  res.sendFile(__dirname + '/public/index/html');
+});
+
+
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router  = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index');
-});
+// router.get('/', (req, res, next) => {
+//   res.render('index');
+// });
+
+// Deleted home page routes because we want angular to handle home page
+// Express should only have API routes
 
 module.exports = router;
